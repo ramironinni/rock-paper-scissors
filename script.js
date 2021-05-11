edae43fdc6b0c14168c05389a7ba3ad1220f5ccb
ramironinni <ramironinni@gmail.com>
2021-05-11 13:20:11 -0300

    Create pseoudocode for the script

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", () => {
+    
+    // Create a variable called elements for the list of elements
+    
+    // Create a function that will randomly return one of the elements from the list
+
+    // Create a function that plays a single round of Rock Paper Scissors
+    // It takes two parameters (playerSelection, computerSelection)
+    // and then return a string that declares the winner of the round
+    
+
+    
+       
+    // Create a function called game(). Use the previous function inside of this one to play a 5 round game
+    // that keeps score and reports a winner or loser at the end.
+
+
+})
