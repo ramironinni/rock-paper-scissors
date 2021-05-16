1cae0b5775019e0ba4d4049bb586fa2bf5a2eb7a
ramironinni <ramironinni@gmail.com>
2021-05-16 11:30:59 -0300

    Fix bug in event listener. Add classes to active selections

--- script.js
+++ script.js
@@ -1,89 +1,108 @@
 window.addEventListener("DOMContentLoaded", () => {
-        
     const elements = ["rock", "paper", "scissors"];
     let roundNumber;
     let playerScore;
     let computerScore;
 
-    function playGame(){
-        const roundResultContainer = document.getElementById("round-result-container");
+    function playGame() {
+        const roundResultContainer = document.getElementById(
+            "round-result-container"
+        );
         roundResultContainer.innerText = "";
 
         const finalScoreDiv = document.getElementById("final-score");
         finalScoreDiv.textContent = "";
-        
+
         playerScore = 0;
         computerScore = 0;
         roundNumber = 0;
 
-        const btnContainer = document.getElementById("player-selection-container");
+        const btnContainer = document.getElementById(
+            "player-selection-container"
+        );
 
         btnContainer.addEventListener("click", (e) => {
             const targetId = e.target.id;
             if (targetId !== btnContainer.id) {
                 roundNumber += 1;
                 if (roundNumber <= 5) {
-                    
-                    const playerSelection = e.target.id;            
-                    
-                    const result = playRound(playerSelection, computerPlay());
-                    const roundResult = showRoundResult(result, playerScore, computerScore);
-                    
+                    e.target.classList.add("active");
+                    setTimeout(() => {
+                        e.target.classList.remove("active");
+                    }, 1000);
+
+                    const playerSelection = e.target.id;
+
+                    const computerPlayResult = computerPlay();
+                    const computerPlaySpan = document.getElementById(
+                        `computer-selection-${computerPlayResult}`
+                    );
+
+                    computerPlaySpan.classList.add("active");
+                    setTimeout(() => {
+                        computerPlaySpan.classList.remove("active");
+                    }, 1000);
+
+                    const result = playRound(
+                        playerSelection,
+                        computerPlayResult
+                    );
+                    const roundResult = showRoundResult(
+                        result,
+                        playerScore,
+                        computerScore
+                    );
+
                     playerScore = roundResult.playerScore;
                     computerScore = roundResult.computerScore;
-                    
+
                     const roundResultMessage = document.createElement("p");
                     roundResultMessage.innerText = roundResult.message;
                     roundResultContainer.appendChild(roundResultMessage);
                 }
-                
+
                 if (roundNumber === 5) {
                     showFinalResult(playerScore, computerScore);
                     askPlayAgain();
-                }            
+                }
             }
-        })
-        
+        });
     }
 
-    function firstGame(isFirstGame){
-
-    }
-
-    function computerPlay(){
+    function computerPlay() {
         const randomElement = Math.floor(Math.random() * elements.length);
         return randomElement;
     }
 
-    function playRound(playerSelection, computerSelection){
+    function playRound(playerSelection, computerSelection) {
         const computerElement = computerSelection;
 
         let playerElement;
         switch (playerSelection) {
-            case "btn-player-rock":
+            case "player-selection-rock":
                 playerElement = 0;
                 break;
-            case "btn-player-paper":
+            case "player-selection-paper":
                 playerElement = 1;
                 break;
-            case "btn-player-scissors":
+            case "player-selection-scissors":
                 playerElement = 2;
-                break;        
+                break;
             default:
                 playerElement = null;
                 break;
         }
 
-        const result = checkResults(playerElement, computerElement)
+        const result = checkResults(playerElement, computerElement);
         return result;
-    }    
+    }
 
-    function checkResults(playerElement, computerElement){
+    function checkResults(playerElement, computerElement) {
         const result = {
             message: null,
-            playerWin: false
+            playerWin: false,
         };
-        
+
         if (playerElement === computerElement) {
             result.message = "You're tied! Try again";
             result.playerWin = null;
@@ -94,61 +113,58 @@ window.addEventListener("DOMContentLoaded", () => {
 
         switch (sum) {
             case 1:
-                result.message = "Paper beats rock."
+                result.message = "Paper beats rock.";
                 if (playerElement === 1) {
-                    result.playerWin = true;                
+                    result.playerWin = true;
                 }
                 break;
             case 2:
-                result.message = "Rock beats scissors."
+                result.message = "Rock beats scissors.";
                 if (playerElement === 0) {
-                    result.playerWin = true;                
+                    result.playerWin = true;
                 }
                 break;
             case 3:
-                result.message = "Scissors beats paper."
+                result.message = "Scissors beats paper.";
                 if (playerElement === 2) {
-                    result.playerWin = true;                
+                    result.playerWin = true;
                 }
-                break;        
-            default:
-                result.message = "It seems there's a bug."
                 break;
-        }      
-        
+            default:
+                result.message = "It seems there's a bug.";
+                break;
+        }
+
         return result;
     }
-        
 
+    function showRoundResult(result, playerScore, computerScore) {
+        if (result.playerWin) {
+            playerScore += 1;
+            result.message += " You win!";
+        } else if (result.playerWin === false) {
+            computerScore += 1;
+            result.message += " You loose!";
+        }
 
-
-    function showRoundResult(result, playerScore, computerScore){
-            if(result.playerWin){
-                playerScore += 1;
-                result.message += " You win!"
-            } else if(result.playerWin === false){
-                computerScore += 1;
-                result.message += " You loose!"
-            }
-
-            const message = `${result.message} Player score: ${playerScore}. Computer score: ${computerScore}`;
-            const roundResult = {
-                message,
-                playerScore: playerScore,
-                computerScore
-            }
-            return roundResult;
+        const message = `${result.message} Player score: ${playerScore}. Computer score: ${computerScore}`;
+        const roundResult = {
+            message,
+            playerScore: playerScore,
+            computerScore,
+        };
+        return roundResult;
     }
 
     function showFinalResult(playerScore, computerScore) {
         const finalScore = document.getElementById("final-score");
 
         let winner;
-        if(playerScore > computerScore){
+        if (playerScore > computerScore) {
             winner = "YOU!";
-        } else if (playerScore < computerScore){
+        } else if (playerScore < computerScore) {
             winner = "COMPUTER!";
-        } else if(playerScore === computerScore){
+        } else if (playerScore === computerScore) {
             winner = "NOBODY! PLAY AGAIN";
         }
 
@@ -158,19 +174,21 @@ window.addEventListener("DOMContentLoaded", () => {
         >> Computer score: ${computerScore}
         WINNER: ${winner}`;
     }
-    
-    function askPlayAgain(){
-        const playAgainContainer = document.getElementById("play-again-container");
+
+    function askPlayAgain() {
+        const playAgainContainer = document.getElementById(
+            "play-again-container"
+        );
         playAgainContainer.classList.remove("hidden");
         playAgainContainer.addEventListener("click", (e) => {
             if (e.target.id === "btn-again-yes") {
                 window.location.reload();
             }
             if (e.target.id === "btn-again-no") {
-                console.log("BYE")
+                console.log("BYE");
             }
-        } )
+        });
     }
-    
+
     playGame();
-})
+});
